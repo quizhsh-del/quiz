4ac4ee273a60f4982b971f8e5730919a1f9fba01
Your Name <your_email@example.com>
2026-02-18 21:18:42 +0530

    update feature

--- Student/views.py
+++ Student/views.py
@@ -5,6 +5,15 @@ import random
 
 
 from .utils import detect_intent, extract_topic, rebuild_mcq_stem
+from django.http import JsonResponse
+from django.contrib import messages
+
+from django.http import JsonResponse 
+
+from django.shortcuts import get_object_or_404, redirect
+from django.contrib import messages
+from django.db.models.deletion import ProtectedError
+from Common.util import  send_emails
 
 
 
@@ -13,17 +22,56 @@ def stlogin(request):
     return render(request,"shome.html",{'forms':reg})
 
 
+# def student_reg(request):
+#     print("hitted")
+#     if request.method == "POST":
+#         form = StudentRegistrationForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             print("Saved successfully")
+#             return redirect('student_login')
+#         else:
+#             print(form.errors)  
+#     else:
+#         form = StudentRegistrationForm()
+
+#     return render(request, 'student/stregister.html', {'form': form})
 def student_reg(request):
-    print("hitted")
+
     if request.method == "POST":
+
         form = StudentRegistrationForm(request.POST)
+
         if form.is_valid():
-            form.save()
-            print("Saved successfully")
+
+            student = form.save()
+
+            # Get form data
+            email = form.cleaned_data.get('email_id')
+            roll_no = form.cleaned_data.get('roll_no')
+            password = form.cleaned_data.get('password')
+
+            subject = "Registration Info"
+            body = f"""Registration on quiz
+Student ID: {roll_no}
+Email: {email} 
+"password : {password}
+"""
+
+            if email:
+                print("Email is:", email)
+                send_emails(receiver=email, subject=subject, body=body)
+                print("Email function executed")
+            messages.success(request, "Registration successful. Please login.")
             return redirect('student_login')
+
         else:
-            print(form.errors)  
+            # Show form errors
+            for field, errors in form.errors.items():
+                for error in errors:
+                    messages.error(request, f"{field}: {error}")
     else:
+
         form = StudentRegistrationForm()
 
     return render(request, 'student/stregister.html', {'form': form})
@@ -254,7 +302,7 @@ def student_quiz_result(request, attempt_id):
     })
 
 
-def student_email_page(request):
+def email_otp(request):
     if request.method == "POST":
         form = StudentRegistrationForm(request.POST)
         email = request.POST.get('email_id')
@@ -266,7 +314,7 @@ def student_email_page(request):
             subject="quiz app password reset"
             otp=random.randint(1000,9999)
             body=f"otp for your password reset = {otp}"
-            sent = send_email(email,subject,body)
+            sent = send_emails(email,subject,body)
             return HttpResponse("otp sended")
         except StudentRegistration.DoesNotExist:
             return HttpResponse("no student in this email id")
@@ -312,3 +360,127 @@ def select_option(request):
 
 
 # nlp code
+
+def password_reset(request):
+
+    stage = request.POST.get("stage", "email")
+
+    if request.method == "POST":
+
+    
+        if stage == "email":
+
+            email = request.POST.get("email")
+
+            try:
+                student = StudentRegistration.objects.get(email_id=email)
+
+                otp = random.randint(100000, 999999)
+
+                request.session["reset_email"] = email
+                request.session["reset_otp"] = str(otp)
+
+                send_emails(
+                    receiver=email,
+                    subject="Password Reset OTP",
+                    body=f"Your OTP for password reset is: {otp}",
+                          )
+
+                messages.success(request, "OTP sent to your email.")
+                return render(request, "passwordreset.html", {"stage": "otp"})
+
+            except StudentRegistration.DoesNotExist:
+                messages.error(request, "Email not registered.")
+
+        
+        elif stage == "otp":
+
+            entered_otp = request.POST.get("otp")
+            session_otp = request.session.get("reset_otp")
+
+            if entered_otp == session_otp:
+                messages.success(request, "OTP verified. Set new password.")
+                return render(request, "passwordreset.html", {"stage": "new_password"})
+            else:
+                messages.error(request, "Invalid OTP.")
+                return render(request, "passwordreset.html", {"stage": "otp"})
+
+        
+        elif stage == "new_password":
+
+            new_password = request.POST.get("new_password")
+            email = request.session.get("reset_email")
+
+            try:
+                student = StudentRegistration.objects.get(email_id=email)
+                student.Password = new_password
+                student.save()
+
+                request.session.pop("reset_otp", None)
+                request.session.pop("reset_email", None)
+
+                messages.success(request, "Password reset successful.")
+                return HttpResponseRedirect(reverse("login_page"))
+
+            except StudentRegistration.DoesNotExist:
+                messages.error(request, "Something went wrong.")
+
+    return render(request, "passwordreset.html", {"stage": "email"})
+def forgot_password(request):
+    if request.method == "POST":
+        email = request.POST.get('email', '').strip().lower()
+        print("EMAIL ENTERED:", email)
+        student = StudentRegistration.objects.filter(email_id__iexact=email).first()
+        print("USER FOUND:", user)
+        if student:
+            otp = str(random.randint(100000, 999999))
+            student.otp = otp
+            student.save()
+            print("OTP GENERATED:", otp)
+            sent_mail(
+                'Password Reset OTP',
+                f'Your OTP is: {otp}',
+                'your_email@gmail.com',
+                [email],
+                fail_silently=False,
+            )
+            request.session['reset_user'] = user.id
+            messages.success(request, "OTP sent to your email.")
+            return redirect('verify_otp')
+        else:
+            messages.error(request, "Email not registered.")
+    role =  request.session.get("role")
+    return render(request, 'forgot_password.html',{'role':role})
+
+def verify_otp(request):
+    if request.method == "POST":
+        entered_otp = request.POST.get('otp')
+        user_id = request.session.get('reset_user')
+        user = Registration.objects.get(id=user_id)
+
+        if user.otp == entered_otp:
+            return redirect('reset_password')
+        else:
+            messages.error(request, "Invalid OTP")
+    role =  request.session.get("role")
+    return render(request, 'verify_otp.html',{'role':role})
+
+def reset_password(request):
+    user_id = request.session.get('reset_user')
+    user = Registration.objects.get(id=user_id)
+
+    if request.method == "POST":
+        p1 = request.POST.get('password')
+        p2 = request.POST.get('confirm_password')
+
+        if p1 != p2:
+            messages.error(request, "Passwords do not match")
+        else:
+            user.password = make_password(p1)
+            user.confirm_password = make_password(p1)
+            user.otp = None
+            user.save()
+            messages.success(request, "Password reset successful")
+            return redirect('login_page')
+    role =  request.session.get("role")
+    return render(request, 'reset_password.html',{'role':role})
